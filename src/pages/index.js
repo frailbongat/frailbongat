055320441b08c682df28be02c5c565a33263dc25
Frail Bongat <frail.bongat@gmail.com>
2021-03-22 02:07:32 +0800

    Added most of the content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,53 @@
 import React from "react"
 
 export default function Home() {
-  return <div className="p-4 bg-pink-400">Hello world!</div>
+  return (
+    <div>
+      <h1>Hey there, I'm Frail 👋</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aut
+        minus cumque explicabo quidem repudiandae tempore eum ducimus nihil
+        provident eveniet possimus molestiae consequuntur sed quia nobis sunt,
+        voluptatibus enim?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aut
+        minus cumque explicabo quidem repudiandae tempore eum ducimus nihil
+        provident eveniet possimus molestiae consequuntur sed quia nobis sunt,
+        voluptatibus enim?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        provident laborum quod culpa maxime corrupti excepturi consectetur
+        adipisci error natus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa cumque
+        inventore possimus, repellat impedit sapiente. A molestias rerum officia
+        tempore in quam voluptatibus? Ut, dolor!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi minima
+        maxime laborum et, dolores ea fugiat maiores consectetur quidem esse
+        tempora voluptas ad atque nostrum, velit modi libero, numquam
+        temporibus.
+      </p>
+      <section>
+        <ul>
+          <li>
+            <a href="#">Github</a>
+            <a href="#">LinkedIn</a>
+            <a href="#">Twitter</a>
+          </li>
+        </ul>
+      </section>
+      <footer>
+        <p>© 2020 Cai Cardenas. No cookies here 🍪</p>
+        <p>
+          Tech in use - Gatsby & Tailwind CSS (So expect this guy to be very
+          fast 😉)
+        </p>
+      </footer>
+    </div>
+  )
 }
