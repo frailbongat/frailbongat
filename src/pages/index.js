a7d04f972e835136652ab6c33cb8baacc92cff7c
Frail Bongat <frail.bongat@gmail.com>
2021-03-22 19:06:47 +0800

    Style: finish layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,50 +2,51 @@ import React from "react"
 
 export default function Home() {
   return (
-    <>
+    <div className="p-6 container max-w-5xl mx-auto sm:pt-32">
       <main>
-        <h1>
-          Hey there, I'm Frail{" "}
+        <h1 className="text-4xl font-extrabold mb-4 sm:text-7xl sm:mb-16">
+          Hi, I'm Frail{" "}
           <span role="img" aria-label="wave">
             👋
           </span>
         </h1>
-        <p>
+        <p className="text-lg mb-4 sm:text-3xl sm:mb-10">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
           aut minus cumque explicabo quidem repudiandae tempore eum ducimus
           nihil provident eveniet possimus molestiae consequuntur sed quia nobis
           sunt, voluptatibus enim?
         </p>
-        <p>
+        <p className="text-lg mb-4 sm:text-3xl sm:mb-10">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
           aut minus cumque explicabo quidem repudiandae tempore eum ducimus
           nihil provident eveniet possimus molestiae consequuntur sed quia nobis
           sunt, voluptatibus enim?
         </p>
-        <p>
+        <p className="text-lg mb-4 sm:text-3xl sm:mb-10">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
           provident laborum quod culpa maxime corrupti excepturi consectetur
           adipisci error natus?
         </p>
-        <p>
+        <p className="text-lg mb-4 sm:text-3xl sm:mb-10">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa cumque
           inventore possimus, repellat impedit sapiente. A molestias rerum
           officia tempore in quam voluptatibus? Ut, dolor!
         </p>
-        <p>
+        <p className="text-lg mb-4 sm:text-3xl sm:mb-10">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi minima
           maxime laborum et, dolores ea fugiat maiores consectetur quidem esse
           tempora voluptas ad atque nostrum, velit modi libero, numquam
           temporibus.
         </p>
       </main>
-      <section>
+      <section className="mb-40">
         <ul>
           <li>
             <a
               href="https://github.com/frailbongat"
               target="_blank"
               rel="noreferrer"
+              className="text-xl font-bold mr-4 sm:text-4xl sm:mr-10"
             >
               Github
             </a>
@@ -53,6 +54,7 @@ export default function Home() {
               href="https://www.linkedin.com/in/frail-bongat-70a71413b/"
               target="_blank"
               rel="noreferrer"
+              className="text-xl font-bold mr-4 sm:text-4xl sm:mr-10"
             >
               LinkedIn
             </a>
@@ -60,6 +62,7 @@ export default function Home() {
               href="https://twitter.com/frailbongat"
               target="_blank"
               rel="noreferrer"
+              className="text-xl font-bold sm:text-4xl"
             >
               Twitter
             </a>
@@ -67,21 +70,19 @@ export default function Home() {
         </ul>
       </section>
       <footer>
-        <p>
-          © 2020 Cai Cardenas. No cookies here{" "}
+        <p className="sm:text-xl">
+          © 2020 Frail Bongat. No cookies here{" "}
           <span role="img" aria-label="cookie">
             🍪
           </span>
         </p>
-        <p>
-          Tech in use - Gatsby & Tailwind CSS (So expect this guy to be very
-          fast{" "}
-          <span role="img" aria-label="wink">
-            😉
+        <p className="sm:text-xl">
+          Made with Gatsby & Tailwind CSS. So expect this guy to be very{" "}
+          <span role="img" aria-label="fast">
+            ⚡
           </span>
-          )
         </p>
       </footer>
-    </>
+    </div>
   )
 }
